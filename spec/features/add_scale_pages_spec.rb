7e6e8f527cae47069c3a32ba23b345c556c0365c
Matt Carlson <matthewthomascarlson@gmail.com>
2016-09-09 11:49:52 -0700

    write integration specs for add and edit scales

---
+++ spec/features/add_scale_pages_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "the add a scale process" do
+  it "adds a new scale" do
+    visit scales_path
+    click_link 'Add a Scale'
+    fill_in 'Name', :with => 'First Scale'
+    click_on 'Create Scale'
+    expect(page).to have_content 'First Scale'
+  end
+
+  it "gives error when no name is entered" do
+    visit new_scale_path
+    click_on 'Create Scale'
+    expect(page).to have_content 'errors'
+  end
+end
---
+++ spec/features/edit_scale_pages_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'the edit a scale process' do
+  it "edits a scale" do
+    scale = Scale.create(:name => "Something")
+    visit scale_path(scale)
+    click_link "Edit"
+    fill_in "Name", :with => "Something cool"
+    click_on "Update Scale"
+    expect(page).to have_content "Something cool"
+  end
+
+  it "gives error when no fields are filled out" do
+    scale = Scale.create(:name => "Something")
+    visit scale_path(scale)
+    click_link "Edit"
+    fill_in "Name", :with => ""
+    click_on "Update Scale"
+    expect(page).to have_content "errors"
+  end
+end
